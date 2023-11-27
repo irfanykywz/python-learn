f26cab29858ffbfd8ffa68f6cb67072947992cce
irfanykywz <irfanyouth@gmail.com>
2023-11-27 12:23:50 +0700

    update

---
+++ Networking/urllib/urlretrieve.py
@@ -0,0 +1,4 @@
+import urllib.request
+
+img_url = 'https://img.freepik.com/free-photo/christmas-lantern-with-fir-branch-decoration-snowy-table-defocused-background-generative-ai_1258-150778.jpg'
+urllib.request.urlretrieve(img_url, "urlretrieve.jpg")
